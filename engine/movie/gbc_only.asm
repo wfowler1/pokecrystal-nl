6c01e801ec3b4cb9602774f83d575d6e0e4e23a6
William Fowler <will.fowler4201@gmail.com>
2024-01-02 21:09:20 -0700

    Translate "Only for Game Boy Color" screen

--- engine/movie/gbc_only.asm
+++ engine/movie/gbc_only.asm
@@ -124,9 +124,9 @@ DrawGBCOnlyGraphic:
 	ret
 
 GBCOnlyString:
-	db   "This Game Pak is"
-	next "designed only for"
-	next "use on the"
+	db   "Deze spelcassette"
+	next "is uitsluitend"
+	next "compatibel met de"
 	next "Game Boy Color.@"
 
 GBCOnlyGFX:
