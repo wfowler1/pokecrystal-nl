5e6e73d099787f0c4be1dc00c4d1481d421ef502
William Fowler <will.fowler4201@gmail.com>
2024-01-02 21:14:25 -0700

    Translate (most of) the PACK

--- engine/items/tmhm.asm
+++ engine/items/tmhm.asm
@@ -368,7 +368,7 @@ TMHM_DisplayPocketItems:
 	push af
 	sub NUM_TMS
 	ld [wTempTMHM], a
-	ld [hl], "H"
+	ld [hl], "V" ; "H"
 	inc hl
 	ld de, wTempTMHM
 	lb bc, PRINTNUM_LEFTALIGN | 1, 2
@@ -451,7 +451,7 @@ PlaceMoveNameAfterTMHMName: ; unreferenced
 	ret
 
 TMHM_CancelString:
-	db "CANCEL@"
+	db "UITGANG@" ; "CANCEL@"
 
 TMHM_GetCurrentPocketPosition:
 	ld hl, wTMsHMs
