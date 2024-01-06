974ed2203702a9e4b4b6e0b3708cdbca24b4257f
William Fowler <will.fowler4201@gmail.com>
2024-01-06 05:47:19 -0700

    Translate Trainer Card

--- engine/menus/trainer_card.asm
+++ engine/menus/trainer_card.asm
@@ -256,9 +256,9 @@ TrainerCard_PrintTopHalfOfCard:
 	ret
 
 .Name_Money:
-	db   "NAME/"
+	db   "NAAM/" ; "NAME/"
 	next ""
-	next "MONEY@"
+	next "GELD@" ; "MONEY@"
 
 .ID_No:
 	db $27, $28, -1 ; ID NO
@@ -294,13 +294,13 @@ TrainerCard_Page1_PrintDexCaught_GameTime:
 
 .Dex_PlayTime:
 	db   "#DEX"
-	next "PLAY TIME@"
+	next "SPEL DUUR@" ; "PLAY TIME@"
 
 .Unused: ; unreferenced
 	db "@"
 
 .Badges:
-	db "  BADGES▶@"
+	db "MEDAILLE▶@" ; "  BADGES▶@"
 
 .StatusTilemap:
 	db $29, $2a, $2b, $2c, $2d, -1
