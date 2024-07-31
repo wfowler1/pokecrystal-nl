83c2ea58d371a5c63d5649262fcadeac8adbadcc
William Fowler <will.fowler4201@gmail.com>
2024-07-31 17:32:16 -0600

    Add English as comments in Abra and Totodile Pokédex data, and make a note about string length.

--- data/pokemon/dex_entries/abra.asm
+++ data/pokemon/dex_entries/abra.asm
@@ -1,10 +1,11 @@
 	db "PSI@" ; species name
 	dw 211, 430 ; height, weight
 
-	db   "Het hypnotiseert"
-	next "zichzelf zodat het"
-	next "weg kan teleporteren"
+	; TODO: Strings too long
+	db   "Het hypnotiseert" ; "It hypnotizes"
+	next "zichzelf zodat het" ; "itself so that it"
+	next "weg kan teleporteren" ; "can teleport away"
 
-	page "wanneer het gevaar"
-	next "voelt, zelfs"
-	next "als het slaapt.@"
+	page "wanneer het gevaar" ; "when it senses"
+	next "voelt, zelfs" ; "danger, even"
+	next "als het slaapt.@" ; "if it is asleep.@"
--- data/pokemon/dex_entries/totodile.asm
+++ data/pokemon/dex_entries/totodile.asm
@@ -1,10 +1,11 @@
 	db "BIG JAW@" ; species name
 	dw 200, 210 ; height, weight
 
-	db   "Dit ruwe beestje"
-	next "bijt naar elk"
-	next "bewegend object dat"
+	; TODO: Strings too long
+	db   "Dit ruwe beestje" ; "This rough critter"
+	next "bijt naar elk" ; "chomps at any"
+	next "bewegend object dat" ; "moving object it"
 
-	page "het ziet. Je rug"
-	next "naar het toekeren"
-	next "wordt niet aanbevolen.@"
+	page "het ziet. Je rug" ; "sees. Turning your"
+	next "naar het toekeren" ; "back on it is not"
+	next "wordt niet aanbevolen.@" ; "recommended.@"
