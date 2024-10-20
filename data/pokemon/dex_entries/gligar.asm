f103f163b1c40b2d3965f7ff0d6d99f91965b0ae
Bastiaan Vroegop <bastiaan.vroegop@gmail.com>
2024-10-20 11:41:05 +0200

    Some new dex entries, early for you to test.

--- data/pokemon/dex_entries/gligar.asm
+++ data/pokemon/dex_entries/gligar.asm
@@ -1,11 +1,11 @@
-	db "FLYSCORPIO@" ; species name
+	db "SCHORPIOEN@" ; species name
 	db 11 ; height
 	dw 648 ; weight
 
-	db   "It builds its nest"
-	next "on a steep cliff."
-	next "When it is done"
+	db   "Bouwt een nest op" ; "It builds its nest"
+	next "een steile klif." ; "on a steep cliff."
+	next "Als het klaar is" ; "When it is done"
 
-	page "gliding, it hops"
-	next "along the ground"
-	next "back to its nest.@"
+	page "met zweven, stapt" ; "gliding, it hops"
+	next "het over de grond" ; "along the ground"
+	next "naar het nest.@" ; "back to its nest.@"
