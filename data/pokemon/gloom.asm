41053f3f946d90bc009cf0cab7e0a82909b27b78
Bastiaan Vroegop <bastiaan.vroegop@gmail.com>
2024-10-20 12:47:56 +0200

    One more for testing!

---
+++ data/pokemon/gloom.asm
@@ -0,0 +1,11 @@
+	db "ONKRUID@" ; species name
+	db 8 ; height
+	dw 86 ; weight
+
+	db   "De geur van de" ; "The smell from its"
+	next "speeksel-achtige" ; "drool-like syrup"
+	next "siroop en de pol-" ; "and the pollen on"
+
+	page "len is zo slecht" ; "its petals is so"
+	next "dat vijanden soms" ; "bad, it may make"
+	next "flauwvallen.@" ; "opponents faint.@"
