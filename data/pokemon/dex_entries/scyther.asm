bc32e075acdf290aa74389247ef6d9081f590c67
BasVroegop <bastiaan.vroegop@gmail.com>
2024-11-03 22:25:52 +0100

    Translated dex entry.

--- data/pokemon/dex_entries/scyther.asm
+++ data/pokemon/dex_entries/scyther.asm
@@ -2,10 +2,10 @@
 	db 15 ; height
 	dw 560 ; weight
 
-	db   "It's very proud of"
-	next "its speed. It"
-	next "moves so fast that"
+	db   "Is erg trots op" ; "It's very proud of"
+	next "hoe snel het is." ; "its speed. It"
+	next "Beweegt zo snel" ; "moves so fast that"
 
-	page "its opponent does"
-	next "not even know what"
-	next "knocked it down.@"
+	page "dat tegenstander" ; "its opponent does"
+	next "niet weet wat het" ; "not even know what"
+	next "neersloeg.@" ; "knocked it down.@"
