90f1d76603d0e6e8758d481983dc29b6c27a5789
BasVroegop <bastiaan.vroegop@gmail.com>
2024-11-11 10:30:30 +0100

    Route 35 done.

--- maps/Route35GoldenrodGate.asm
+++ maps/Route35GoldenrodGate.asm
@@ -64,8 +64,8 @@ RandyScript:
 
 GiftSpearowMail:
 	db FLOWER_MAIL
-	db   "DARK CAVE leads"
-	next "to another road@"
+	db   "DONKERE GROT leidt" ; "DARK CAVE leads"
+	next "naar andere weg." ; "to another road@"
 
 GiftSpearowName:
 	db "KENYA@"
@@ -95,104 +95,104 @@ Route35GoldenrodGateFisherScript:
 	jumptextfaceplayer Route35GoldenrodGateFisherText
 
 Route35GoldenrodGateRandyAskTakeThisMonToMyFriendText:
-	text "Excuse me, kid!"
-	line "Can you do a guy"
-	cont "a favor?"
+	text "Pardon, koter!" ; "Excuse me, kid!"
+	line "Kun je iets voor" ; "Can you do a guy"
+	cont "me doen?" ; "a favor?"
 
-	para "Can you take this"
-	line "#MON with MAIL"
-	cont "to my friend?"
+	para "Kun je deze #-" ; "Can you take this"
+	line "MON met post naar" ; "#MON with MAIL"
+	cont "iemand brengen?" ; "to my friend?"
 
-	para "He's on ROUTE 31."
+	para "Op ROUTE 31, dus." ; "He's on ROUTE 31."
 	done
 
 Route35GoldenrodGateRandyThanksText:
-	text "You will? Perfect!"
-	line "Thanks, kid!"
+	text "Doe je het? Top!" ; "You will? Perfect!"
+	line "Bedankt, koter!" ; "Thanks, kid!"
 
-	para "My pal's a chubby"
-	line "guy who snoozes"
-	cont "all the time."
+	para "Mijn maat is een" ; "My pal's a chubby"
+	line "mollige gast die" ; "guy who snoozes"
+	cont "vaak dutjes doet." ; "all the time."
 
-	para "You'll recognize"
-	line "him right away!"
+	para "Je herkent hem" ; "You'll recognize"
+	line "vast meteen!" ; "him right away!"
 	done
 
 Route35GoldenrodGatePlayerReceivedAMonWithMailText:
-	text "<PLAYER> received a"
-	line "#MON with MAIL."
+	text "<PLAYER> ontving" ; "<PLAYER> received a"
+	line "#MON met POST." ; "#MON with MAIL."
 	done
 
 Route35GoldenrodGateRandyWeirdTreeBlockingRoadText:
-	text "You can read it,"
-	line "but don't lose it!"
-	cont "ROUTE 31!"
+	text "Lees het gerust," ; "You can read it,"
+	line "maar niet kwijt-" ; "but don't lose it!"
+	cont "raken! ROUTE 31!" ; "ROUTE 31!"
 
-	para "Oh, yeah. There"
-	line "was a weird tree"
-	cont "blocking the road."
+	para "Oh, ja. Een rare" ; "Oh, yeah. There"
+	line "boom blokkeerde de" ; "was a weird tree"
+	cont "weg." ; "blocking the road."
 
-	para "I wonder if it's"
-	line "been cleared?"
+	para "Zou die al zijn" ; "I wonder if it's"
+	line "weggehaald?" ; "been cleared?"
 	done
 
 Route35GoldenrodGateRandyCantCarryAnotherMonText:
-	text "You can't carry"
-	line "another #MON…"
+	text "Je kunt niet meer" ; "You can't carry"
+	line "#MON meenemen…" ; "another #MON…"
 	done
 
 Route35GoldenrodGateRandyOhNeverMindThenText:
-	text "Oh… Never mind,"
-	line "then…"
+	text "Oh… Laat maar," ; "Oh… Never mind,"
+	line "dan…" ; "then…"
 	done
 
 Route35GoldenrodGateRandySomethingForYourTroubleText:
-	text "Thanks, kid! You"
-	line "made the delivery"
-	cont "for me!"
+	text "Bedankt, koter! Je" ; "Thanks, kid! You"
+	line "hebt het voor me" ; "made the delivery"
+	cont "bezorgd!" ; "for me!"
 
-	para "Here's something"
-	line "for your trouble!"
+	para "Hier, iets voor" ; "Here's something"
+	line "de moeite." ; "for your trouble!"
 	done
 
 Route35GoldenrodGateRandyMyPalWasSnoozingRightText:
-	text "My pal was snooz-"
-	line "ing, right? Heh,"
-	cont "what'd I say?"
+	text "Mijn maat lag te" ; "My pal was snooz-"
+	line "pitten, of niet?" ; "ing, right? Heh,"
+	cont "Dacht ik al!" ; "what'd I say?"
 	done
 
 Route35GoldenrodGatePokefanFText:
-	text "A strange tree is"
-	line "blocking the road."
+	text "Een vreemde boom" ; "A strange tree is"
+	line "blokkeert de weg." ; "blocking the road."
 
-	para "It wriggles around"
-	line "if you talk to it."
+	para "Hij wiebelt als je" ; "It wriggles around"
+	line "er tegen praat." ; "if you talk to it."
 
-	para "I heard it became"
-	line "wild when someone"
+	para "Kennelijk werd ie" ; "I heard it became"
+	line "wild toen iemand" ; "wild when someone"
 
-	para "watered it with a"
-	line "SQUIRTBOTTLE."
+	para "er met een SPUIT-" ; "watered it with a"
+	line "FLES op sproeide." ; "SQUIRTBOTTLE."
 	done
 
 Route35GoldenrodGatePokefanFText_FoughtSudowoodo:
-	text "I like the #MON"
-	line "Lullaby they play"
-	cont "on the radio."
+	text "Ik hou van het" ; "I like the #MON"
+	line "#MON-Slaaplied" ; "Lullaby they play"
+	cont "op de radio." ; "on the radio."
 	done
 
 Route35GoldenrodGateFisherText:
-	text "I wonder how many"
-	line "kinds of #MON"
+	text "Hoeveel soorten" ; "I wonder how many"
+	line "#MON zouden er" ; "kinds of #MON"
 
-	para "there are in the"
-	line "world."
+	para "op de wereld" ; "there are in the"
+	line "zijn?" ; "world."
 
-	para "Three years ago,"
-	line "PROF.OAK said that"
+	para "Drie jaar geleden" ; "Three years ago,"
+	line "zei PROF.OAK dat" ; "PROF.OAK said that"
 
-	para "there were 150"
-	line "different kinds."
+	para "er 150 soorten" ; "there were 150"
+	line "bestaan." ; "different kinds."
 	done
 
 Route35GoldenrodGate_MapEvents:
