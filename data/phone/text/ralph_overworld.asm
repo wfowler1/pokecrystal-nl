15a0d7d01060fb55fb181f1fb65b286ff1e0bdf4
BasVroegop <bastiaan.vroegop@gmail.com>
2024-11-17 09:21:03 +0100

    Ralph translated.

--- data/phone/text/ralph_overworld.asm
+++ data/phone/text/ralph_overworld.asm
@@ -1,58 +1,60 @@
 RalphAskNumber1Text:
-	text "Don't young people"
-	line "fish anymore?"
+	text "Vissen jongeren" ; "Don't young people"
+	line "niet meer?" ; "fish anymore?"
 
-	para "I've seen rare"
-	line "#MON while I've"
-	cont "been fishing…"
+	para "Ik heb tijdens het" ; "I've seen rare"
+	line "vissen zeldzame" ; "#MON while I've"
+	cont "#MON gezien…" ; "been fishing…"
 
-	para "You young people"
-	line "like that, right?"
+	para "Jullie jonkies" ; "You young people"
+	line "vinden dat leuk," ; "like that, right?"
+	cont "toch?"
 
-	para "Want to trade"
-	line "phone numbers?"
+	para "Wil je telefoon-" ; "Want to trade"
+	line "nummers ruilen?" ; "phone numbers?"
 
-	para "I'll let you know"
-	line "if I see anything."
+	para "Ik laat het weten" ; "I'll let you know"
+	line "als ik wat zie." ; "if I see anything."
 	done
 
 RalphAskNumber2Text:
-	text "Hm? So you do want"
-	line "me to phone if I"
-	cont "see rare #MON?"
+	text "Hm? Wil je dat ik" ; "Hm? So you do want"
+	line "bel als ik zeld-" ; "me to phone if I"
+	cont "zame #MON zie?" ; "see rare #MON?"
 	done
 
 RalphNumberAcceptedText:
-	text "Yep, phone if I"
-	line "see rare #MON."
+	text "Yep, bel als ik" ; "Yep, phone if I"
+	line "zeldzame #MON" ; "see rare #MON."
+	cont "zie."
 
-	para "Don't worry, I"
-	line "won't forget!"
+	para "Geen zorgen, ik" ; "Don't worry, I"
+	line "vergeet het niet!" ; "won't forget!"
 	done
 
 RalphNumberDeclinedText:
-	text "Oh…"
-	line "My own kids won't"
+	text "Oh… Mijn eigen" ; "Oh…"
+	line "kinderen willen" ; "My own kids won't"
 
-	para "even give me their"
-	line "phone numbers…"
+	para "niet eens hun" ; "even give me their"
+	line "nummers geven…" ; "phone numbers…"
 
-	para "Is that how young"
-	line "people are today?"
+	para "Is dat hoe jonge" ; "Is that how young"
+	line "mensen nu zijn?" ; "people are today?"
 	done
 
 RalphPhoneFullText:
-	text "Your phone's"
-	line "memory is full."
+	text "Je telefoon-" ; "Your phone's"
+	line "geheugen is vol." ; "memory is full."
 
-	para "It seems that"
-	line "young people all"
-	cont "have #GEAR."
+	para "Alle jongelui" ; "It seems that"
+	line "hebben nu een" ; "young people all"
+	cont "#GEAR." ; "have #GEAR."
 	done
 
 RalphRematchText:
-	text "Hey, kid!"
+	text "He, kind!" ; "Hey, kid!"
 
-	para "No losing for me"
-	line "this day!"
+	para "Ik ga vandaag niet" ; "No losing for me"
+	line "verliezen!" ; "this day!"
 	done
