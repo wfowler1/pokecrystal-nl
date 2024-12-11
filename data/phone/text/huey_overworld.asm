0666adbf5e2f45ada403b38c6092dff383443fa9
BasVroegop <bastiaan.vroegop@gmail.com>
2024-12-11 09:03:43 +0100

    Huey translated

--- data/phone/text/huey_overworld.asm
+++ data/phone/text/huey_overworld.asm
@@ -1,80 +1,81 @@
 HueyAskNumber1Text:
-	text "Cool! That's the"
-	line "first time I've"
-	cont "lost in a while!"
+	text "Cool! Het was even" ; "Cool! That's the"
+	line "geleden dat ik een" ; "first time I've"
+	cont "gevecht verloor!" ; "lost in a while!"
 
-	para "Hey, give me your"
-	line "phone number."
+	para "He, geef mij je" ; "Hey, give me your"
+	line "telefoonnummer." ; "phone number."
 
-	para "You'd be fun to"
-	line "battle, so I'll"
+	para "Het lijkt me leuk" ; "You'd be fun to"
+	line "om nog eens te" ; "battle, so I'll"
 
-	para "call you when I"
-	line "get stronger!"
+	para "vechten, ik bel" ; "call you when I"
+	line "als ik sterk ben!" ; "get stronger!"
 	done
 
 HueyAskNumber2Text:
-	text "Hey, give me your"
-	line "phone number."
+	para "He, geef mij je" ; "Hey, give me your"
+	line "telefoonnummer." ; "phone number."
 
-	para "You'd be fun to"
-	line "battle, so I'll"
+	para "Het lijkt me leuk" ; "You'd be fun to"
+	line "om nog eens te" ; "battle, so I'll"
 
-	para "call you when I"
-	line "get stronger!"
+	para "vechten, ik bel" ; "call you when I"
+	line "als ik sterk ben!" ; "get stronger!"
 	done
 
 HueyNumberAcceptedText:
-	text "Hey, let's battle"
-	line "again!"
+	text "He, laten we nog" ; "Hey, let's battle"
+	line "eens vechten!" ; "again!"
 	done
 
 HueyNumberDeclinedText:
-	text "Looks like you're"
-	line "a wimp…"
+	text "Zo te zien ben je" ; "Looks like you're"
+	line "een watje…" ; "a wimp…"
 	done
 
 HueyPhoneFullText:
-	text "Hey! Your phone's"
-	line "already full!"
+	text "He! Je telefoon is" ; "Hey! Your phone's"
+	line "al vol!" ; "already full!"
 	done
 
 HueyRematchText:
-	text "Hey! I'm tired of"
-	line "waiting for you!"
+	text "He! Ik ben klaar" ; "Hey! I'm tired of"
+	line "met wachten op je!" ; "waiting for you!"
 	done
 
 HueyPackFullText:
-	text "Oh… Your PACK's"
-	line "full. I'll give it"
-	cont "to you later."
+	text "Oh… Je TAS zit" ; "Oh… Your PACK's"
+	line "vol. Ik geef het" ; "full. I'll give it"
+	cont "je later wel." ; "to you later."
 	done
 
 HueyRematchGiftText:
-	text "Man! You're as"
-	line "tough as ever!"
+	text "Man! Je bent zo" ; "Man! You're as"
+	line "sterk als altijd!" ; "tough as ever!"
 
-	para "I've battled you"
-	line "over and over, but"
+	para "Ik heb heel vaak" ; "I've battled you"
+	line "tegen je geknokt," ; "over and over, but"
 
-	para "<PLAYER>, I haven't"
-	line "won once."
+	para "<PLAYER>, maar nog" ; "<PLAYER>, I haven't"
+	line "nooit gewonnen." ; "won once."
 
-	para "I bought tons of"
-	line "items to toughen"
+	para "Ik heb veel voor-" ; "I bought tons of"
+	line "werpen gekocht om" ; "items to toughen"
 
-	para "up my #MON, but"
-	line "to no avail…"
+	para "mijn #MON sterker" ; "up my #MON, but"
+	line "te maken, zonder" ; "to no avail…"
+	cont "succes…"
 
-	para "Items alone aren't"
-	line "the answer."
+	para "Alleen voorwerpen" ; "Items alone aren't"
+	line "is niet genoeg." ; "the answer."
 
-	para "That must be what"
-	line "it is…"
+	para "Dat is vast het" ; "That must be what"
+	line "probleem…" ; "it is…"
 
-	para "Here, take this,"
+	para "Hier, neem dit," ; "Here, take this,"
 	line "<PLAYER>."
 
-	para "I won't be needing"
-	line "it anymore…"
+	para "Ik heb het niet" ; "I won't be needing"
+	line "langer nodig…" ; "it anymore…"
 	done
