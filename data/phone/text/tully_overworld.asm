54e0667c1ee8abf75af35cbc3e2c5a9373548d18
BasVroegop <bastiaan.vroegop@gmail.com>
2024-12-14 19:48:54 +0100

    Fixes.

--- data/phone/text/tully_overworld.asm
+++ data/phone/text/tully_overworld.asm
@@ -1,73 +1,73 @@
 TullyAskNumber1Text:
-	text "He, probeer jij" "Hey, you're trying"
-	line "ook de ultieme" "to be the ultimate"
+	text "He, probeer jij" ; "Hey, you're trying"
+	line "ook de ultieme" ; "to be the ultimate"
 
-	para "trainer te worden?" "trainer too? Then"
-	line "Dan zijn we maten!" "we're comrades!"
+	para "trainer te worden?" ; "trainer too? Then"
+	line "Dan zijn we maten!" ; "we're comrades!"
 
-	para "Als ik voorwerpen" "If I find any more"
-	line "bij het water spot" "items by the"
+	para "Als ik voorwerpen" ; "If I find any more"
+	line "bij het water spot" ; "items by the"
 
-	para "mag je wel wat van" "water, I'll give"
-	line "me hebben." "you some."
+	para "mag je wel wat van" ; "water, I'll give"
+	line "me hebben." ; "you some."
 
-	para "Geef maar je" "Just give me your"
-	line "telefoonnummer." "phone number."
+	para "Geef maar je" ; "Just give me your"
+	line "telefoonnummer." ; "phone number."
 	done
 
 TullyAskNumber2Text:
-	text "Als ik voorwerpen" "If I find any more"
-	line "bij het water spot" "items by the"
+	text "Als ik voorwerpen" ; "If I find any more"
+	line "bij het water spot" ; "items by the"
 	
-	para "mag je wel wat van" "water, I'll give"
-	line "me hebben." "you some."
+	para "mag je wel wat van" ; "water, I'll give"
+	line "me hebben." ; "you some."
 	
-	para "Geef maar je" "Just give me your"
-	line "telefoonnummer." "phone number."
+	para "Geef maar je" ; "Just give me your"
+	line "telefoonnummer." ; "phone number."
 	done
 
 TullyNumberAcceptedText:
-	text "You'll be hearing"
-	line "from me if I find"
-	cont "something good."
+	text "Je hoort van me" ; "You'll be hearing"
+	line "als ik wat moois" ; "from me if I find"
+	cont "tegenkom." ; "something good."
 	done
 
 TullyNumberDeclinedText:
-	text "Is that so? Then"
-	line "I'll just have to"
-	cont "use them myself."
+	text "Is dat zo? Dan" ; "Is that so? Then"
+	line "moet ik het zelf" ; "I'll just have to"
+	cont "maar gebruiken." ; "use them myself."
 	done
 
 TullyPhoneFullText:
-	text "Your phone list is"
-	line "all filled up."
+	text "Je telefoonlijst" ; "Your phone list is"
+	line "is helemaal vol." ; "all filled up."
 
-	para "Come back if you"
-	line "make room for me."
+	para "Kom terug als je" ; "Come back if you"
+	line "plek hebt gemaakt." ; "make room for me."
 	done
 
 TullyRematchText:
-	text "I've been doing"
-	line "more than just"
+	text "Ik heb meer dan" ; "I've been doing"
+	line "alleen gevist na" ; "more than just"
 
-	para "fishing since we"
-	line "last met."
+	para "onze ontmoeting" ; "fishing since we"
+	line "laatst." ; "last met."
 
-	para "You're in for a"
-	line "big surprise!"
+	para "Ik ga je flink" ; "You're in for a"
+	line "verrassen!" ; "big surprise!"
 	done
 
 TullyGiftText:
-	text "Hey, there you"
-	line "are!"
+	text "He, daar ben je" ; "Hey, there you"
+	line "dan!" ; "are!"
 
-	para "Here's a gift, as"
-	line "promised!"
+	para "Hier een cadeau," ; "Here's a gift, as"
+	line "zoals beloofd!" ; "promised!"
 	done
 
 TullyPackFullText:
-	text "Your PACK's full?"
+	text "Je TAS is vol?" ; "Your PACK's full?"
 
-	para "Come back later--"
-	line "that'll do it."
+	para "Kom later maar--" ; "Come back later--"
+	line "dat is prima." ; "that'll do it."
 	done
