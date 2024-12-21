b6757ddaf0d64d166bdff7796cf590b1fd40e344
BasVroegop <bastiaan.vroegop@gmail.com>
2024-12-21 12:36:40 +0100

    Reena translated.

--- data/phone/text/reena_overworld.asm
+++ data/phone/text/reena_overworld.asm
@@ -1,45 +1,45 @@
 ReenaAskNumber1Text:
-	text "Not bad…"
-	line "It's something to"
+	text "Niet slecht…" ; "Not bad…"
+	line "Mij verslaan is" ; "It's something to"
 
-	para "beat me, even by"
-	line "an unlikely fluke…"
+	para "bijzonder, zelfs" ; "beat me, even by"
+	line "door een foutje." ; "an unlikely fluke…"
 
-	para "I like you! Give"
-	line "me your number!"
+	para "Ik mag je! Geef me" ; "I like you! Give"
+	line "je nummer!" ; "me your number!"
 
-	para "You can be my"
-	line "practice partner!"
+	para "Je wordt mijn" ; "You can be my"
+	line "oefenpartner!" ; "practice partner!"
 	done
 
 ReenaAskNumber2Text:
-	text "Give me your phone"
-	line "number!"
+	text "Geef mij je" ; "Give me your phone"
+	line "telefoonnummer!" ; "number!"
 
-	para "You are going to"
-	line "be my practice"
-	cont "partner!"
+	para "Jij bent voortaan" ; "You are going to"
+	line "mijn vaste oefen-" ; "be my practice"
+	cont "partner!" ; "partner!"
 	done
 
 ReenaNumberAcceptedText:
-	text "Don't be too proud"
-	line "about beating me!"
+	text "Wees niet te trots" ; "Don't be too proud"
+	line "omdat je won!" ; "about beating me!"
 
-	para "It was a fluke!"
+	para "Het was een foutje!" ; "It was a fluke!"
 	done
 
 ReenaNumberDeclinedText:
-	text "I don't believe"
-	line "it! You're going"
-	cont "to regret this!"
+	text "Ik geloof 't niet!" ; "I don't believe"
+	line "Hier ga jij spijt" ; "it! You're going"
+	cont "van krijgen!" ; "to regret this!"
 	done
 
 ReenaPhoneFullText:
-	text "What? Your phone"
-	line "list has no room!"
+	text "Wat? Je telefoon" ; "What? Your phone"
+	line "heeft geen plek!" ; "list has no room!"
 	done
 
 ReenaRematchText:
-	text "What took you?"
-	line "Start right away!"
+	text "Waar bleef je?" ; "What took you?"
+	line "Start meteen!" ; "Start right away!"
 	done
