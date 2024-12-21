bd85bc7ab141a36021037ea1b90eb3b3f829662c
BasVroegop <bastiaan.vroegop@gmail.com>
2024-12-21 08:43:37 +0100

    Missing semicolons added.

--- data/phone/text/kenji_callee.asm
+++ data/phone/text/kenji_callee.asm
@@ -1,44 +1,44 @@
 KenjiAnswerPhoneText:
 	text_ram wStringBuffer3 ; MaxLength 5
-	text "hier…" " here…"
+	text "hier…" ; " here…"
 
-	para "<PLAYER>! Fraaie" "<PLAYER>! Beautiful"
-	line "ochtend, he?" "morning, hey?"
+	para "<PLAYER>! Fraaie" ; "<PLAYER>! Beautiful"
+	line "ochtend, he?" ; "morning, hey?"
 	done
 
 KenjiAnswerPhoneDayText:
 	text_ram wStringBuffer3 ; MaxLength 5
-	text "hier…" " here…"
+	text "hier…" ; " here…"
 
-	para "<PLAYER>! Lekker" "<PLAYER>! Beautiful"
-	line "weertje, he?" "weather, hey?"
+	para "<PLAYER>! Lekker" ; "<PLAYER>! Beautiful"
+	line "weertje, he?" ; "weather, hey?"
 	done
 
 KenjiAnswerPhoneNiteText:
 	text_ram wStringBuffer3 ; MaxLength 5
-	text "hier…" " here…"
+	text "hier…" ; " here…"
 
-	para "<PLAYER>! Fraai" "<PLAYER>! Beautiful"
-	line "maanlicht, he?" "moonlight, hey?"
+	para "<PLAYER>! Fraai" ; "<PLAYER>! Beautiful"
+	line "maanlicht, he?" ; "moonlight, hey?"
 	done
 
 KenjiGreetText:
-	text "He, <PLAYER>!" "Hey, <PLAYER>!"
-	line "Dit is @" "This is @"
+	text "He, <PLAYER>!" ; "Hey, <PLAYER>!"
+	line "Dit is @" ; "This is @"
 	text_ram wStringBuffer3 ; MaxLength 5
 	text "!"
 	done
 
 KenjiGreetDayText:
-	text "He, <PLAYER>!" "Hey, <PLAYER>!"
-	line "Dit is @" "This is @"
+	text "He, <PLAYER>!" ; "Hey, <PLAYER>!"
+	line "Dit is @" ; "This is @"
 	text_ram wStringBuffer3 ; MaxLength 5
 	text "!"
 	done
 
 KenjiGreetNiteText:
-	text "He, <PLAYER>!" "Hey, <PLAYER>!"
-	line "Dit is @" "This is @"
+	text "He, <PLAYER>!" ; "Hey, <PLAYER>!"
+	line "Dit is @" ; "This is @"
 	text_ram wStringBuffer3 ; MaxLength 5
 	text "!"
 	done
