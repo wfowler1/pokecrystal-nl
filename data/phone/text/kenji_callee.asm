c7240218f6a1828fde952130bdbad9e14e7c863a
BasVroegop <bastiaan.vroegop@gmail.com>
2024-12-21 08:42:36 +0100

    Kenji translated.

--- data/phone/text/kenji_callee.asm
+++ data/phone/text/kenji_callee.asm
@@ -1,44 +1,44 @@
 KenjiAnswerPhoneText:
 	text_ram wStringBuffer3 ; MaxLength 5
-	text " here…"
+	text "hier…" " here…"
 
-	para "<PLAYER>! Beautiful"
-	line "morning, hey?"
+	para "<PLAYER>! Fraaie" "<PLAYER>! Beautiful"
+	line "ochtend, he?" "morning, hey?"
 	done
 
 KenjiAnswerPhoneDayText:
 	text_ram wStringBuffer3 ; MaxLength 5
-	text " here…"
+	text "hier…" " here…"
 
-	para "<PLAYER>! Beautiful"
-	line "weather, hey?"
+	para "<PLAYER>! Lekker" "<PLAYER>! Beautiful"
+	line "weertje, he?" "weather, hey?"
 	done
 
 KenjiAnswerPhoneNiteText:
 	text_ram wStringBuffer3 ; MaxLength 5
-	text " here…"
+	text "hier…" " here…"
 
-	para "<PLAYER>! Beautiful"
-	line "moonlight, hey?"
+	para "<PLAYER>! Fraai" "<PLAYER>! Beautiful"
+	line "maanlicht, he?" "moonlight, hey?"
 	done
 
 KenjiGreetText:
-	text "Hey, <PLAYER>!"
-	line "This is @"
+	text "He, <PLAYER>!" "Hey, <PLAYER>!"
+	line "Dit is @" "This is @"
 	text_ram wStringBuffer3 ; MaxLength 5
 	text "!"
 	done
 
 KenjiGreetDayText:
-	text "Hey, <PLAYER>!"
-	line "This is @"
+	text "He, <PLAYER>!" "Hey, <PLAYER>!"
+	line "Dit is @" "This is @"
 	text_ram wStringBuffer3 ; MaxLength 5
 	text "!"
 	done
 
 KenjiGreetNiteText:
-	text "Hey, <PLAYER>!"
-	line "This is @"
+	text "He, <PLAYER>!" "Hey, <PLAYER>!"
+	line "Dit is @" "This is @"
 	text_ram wStringBuffer3 ; MaxLength 5
 	text "!"
 	done
