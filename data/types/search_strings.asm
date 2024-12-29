5680b638d99b92796b0f73679be40dba127f6d98
BasVroegop <bastiaan.vroegop@gmail.com>
2024-12-29 22:14:04 +0100

    Playtest fixes.

--- data/types/search_strings.asm
+++ data/types/search_strings.asm
@@ -12,7 +12,7 @@ PokedexTypeSearchStrings:
 	db "  GIF   @" ; " POISON @"
 	db " GROND  @" ; " GROUND @"
 	db "VLIEGEN @" ; " FLYING @"
-	db "MENTAAL @" ; "PSYCHIC @"
+	db "PSYCH.  @" ; "PSYCHIC @"
 	db " INSECT @" ; "  BUG   @"
 	db " STEEN  @" ; "  ROCK  @"
 	db " GEEST  @" ; " GHOST  @"
